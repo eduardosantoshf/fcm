227f30c8531e7809abcf3745c6feaace555dbfd8
BrunosBastos <Brunosb@ua.pt>
2021-10-26 18:45:06 +0100

    initial commit

---
+++ src/fcm.py
@@ -0,0 +1,99 @@
+import sys
+
+times = {}
+
+def get_alphabet(filename):
+    with open(filename, "r") as f:
+        alphabet = list(set(f.read()))
+
+    return alphabet
+
+def calculate_table_space(alphabet_size, k):
+    # calculates the amount of RAM used by the table
+    # assuming 16 bits integers
+    # result in MB
+    return (alphabet_size ** k) * alphabet_size * 16 / 8 / 1024 / 1024 
+
+
+def init_table(alphabet, k):
+
+    space = calculate_table_space(len(alphabet), k)
+    threshold = 0.5
+    if space >= threshold:
+        table = {}
+    else:
+        table = [[0] * len(alphabet) for _ in range(len(alphabet) ** k)]
+    return table
+
+def get_table_index(seq, alphabet):
+    index = 0
+    n = 0
+    for i in reversed(seq):
+        index += len(alphabet)**n * alphabet.index(i)
+        n += 1
+    return index
+
+def fill_table(table, alphabet, k, filename):
+    global times
+
+    with open(filename, "r") as f:
+        seq = f.read(k)
+        while True:
+            c = f.read(1)
+            if not c:
+                break
+            if c not in times:
+                times[c] = 1
+            else:
+                times[c] += 1
+
+            #table[get_table_index(seq, alphabet)][alphabet.index(c)] += 1 
+            inc_table_index(seq, alphabet, table, c)
+            seq = seq[1:] + c
+    
+    return table
+
+def inc_table_index(seq, alphabet, table, c):
+
+    if type(table) == list:
+        table[get_table_index(seq, alphabet)][alphabet.index(c)] += 1 
+    else:
+        if seq not in table:
+            table[seq] = {c: 1}
+        else:
+            if c not in table[seq]:
+                table[seq][c] = 1
+            else:
+                table[seq][c] += 1
+
+def get_table_row(seq, alphabet, table):
+    global times
+    if type(table) == list:
+        return table[get_table_index(seq, alphabet)]
+    else:
+        if seq in table:
+            temp = []
+            for c in alphabet:
+                if c in table[seq]:
+                    temp.append(table[seq][c])
+                else:
+                    temp.append(0)
+            return temp
+        else:
+            
+            temp = []
+            for c in alphabet:
+                temp.append(times[c])
+            return temp
+            
+            #return [0 for _ in range(len(alphabet))]
+
+def main():
+    filename = sys.argv[1]
+    k = 1 if len(sys.argv) < 2 else int(sys.argv[2])
+    alphabet = get_alphabet(filename)
+    fill_table(init_table(alphabet, k), alphabet, k, filename)
+
+
+if __name__== "__main__":
+    main()
---
+++ src/generator.py
@@ -0,0 +1,33 @@
+from fcm import get_alphabet, fill_table, init_table, get_table_index, get_table_row
+import sys
+import random
+
+def main():
+    filename = sys.argv[1]
+    k = 1 if len(sys.argv) < 2 else int(sys.argv[2])
+    alphabet = get_alphabet(filename)
+    table = fill_table(init_table(alphabet, k), alphabet, k, filename)
+
+    seq = ""
+    for i in range(k):
+        seq += alphabet[random.randint(0, len(alphabet) - 1)]
+
+    size = 10000
+    text = seq
+    alpha = 1
+    for i in range(size):
+        row = get_table_row(seq, alphabet, table)
+        total = sum(row) + len(alphabet) * alpha
+        r = random.randint(0, total - 1)
+        for i, v in enumerate(row):
+            r -= v + alpha
+            if r <= 0:
+                text += alphabet[i]
+                break
+        
+        seq = seq[1:] + text[-1]
+
+    print(text)
+
+if __name__ == "__main__":
+    main()
