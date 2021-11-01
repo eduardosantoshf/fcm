aa20676c24b51a0c2d9c499fe44c7c444542632a
BrunosBastos <Brunosb@ua.pt>
2021-11-01 23:33:45 +0000

    added entropy for 2D table

--- src/fcm_2.py
+++ src/fcm_2.py
@@ -3,16 +3,18 @@ import math
 
 class Fcm:
     
-    def __init__(self, filename, k):
+    def __init__(self, filename, k, alpha):
 
         # initial variables
         self.filename = filename
         self.k = k
+        self.alpha = alpha
         self.times = dict()
 
         self.alphabet = self.get_alphabet()
         self.alphabet_size = len(self.alphabet)
 
+        self.is_hash = False
         # fill table with number of occurences
         self.filled_table = self.fill_table(self.init_table())
 
@@ -32,8 +34,10 @@ class Fcm:
         space = self.calculate_table_space()
         threshold = 0.5
         if space >= threshold:
+            self.is_hash = True
             table = {}
         else:
+            self.is_hash = False
             table = [[0] * self.alphabet_size for _ in range(self.alphabet_size ** self.k)]
         return table
 
@@ -64,7 +68,7 @@ class Fcm:
 
     def inc_table_index(self, seq, table, c):
 
-        if type(table) == list:
+        if not self.is_hash:
             table[self.get_table_index(seq)][self.alphabet.index(c)] += 1 
         else:
             if seq not in table:
@@ -85,7 +89,7 @@ class Fcm:
 
     def get_table_row(self, seq, table):
 
-        if type(table) == list:
+        if not self.is_hash:
             return table[self.get_table_index(seq, self.alphabet)]
         else:
             if seq in table:
@@ -97,31 +101,28 @@ class Fcm:
                         temp.append(0)
                 return temp
             else:
-                
-                temp = []
-                for c in self.alphabet:
-                    temp.append(self.times[c])
-                return temp
-                
-                #return [0 for _ in range(len(alphabet))]
+                return [0 for _ in range(len(alphabet))]
 
     def calculate_each_probability(self, row):
         # list of probabilities for each letter to appear after context
         prob_list = []
-        alpha = 0.1
 
-        # sum of the occurences of the row
-        for x in row:
-            total = 0
-            for k in row:
-                total += row[k]
-            
-            prob_list.append((row[x] + alpha) / (total + (abs(total) * alpha)))
+        if self.is_hash:
+            # sum of the occurences of the row
+            total = sum(row.values()) + self.alphabet_size * self.alpha
 
-            #probability of each letter
-            #prob_list.append(row[x] / total)
+            # some symbols might not be in the hashtable,
+            # so we need to iterate the alphabet and add alpha to every symbol
+            for c in self.alphabet:
+                if c in row:
+                    prob_list.append((row[c] + self.alpha) / total)
+                else:
+                    prob_list.append(self.alpha/total)
+            return prob_list
+        else:
+            total = sum(row) + self.alphabet_size * self.alpha
+            return [(c + self.alpha)/total for c in row]
 
-        return prob_list
 
     def calculate_each_entropy(self, prob_list):
         entropy = 0.0
@@ -133,34 +134,46 @@ class Fcm:
         return - entropy
 
     def get_sum_table_values(self):
-
-        # sum of all probabilities of the table
-        for k in self.filled_table:
-            for j in self.filled_table[k]:
-                self.total_counter += self.filled_table[k][j]
+        
+        if self.is_hash:
+            total = 0
+            # sum of all probabilities of the table
+            for k in self.filled_table:
+                for j in self.filled_table[k]:
+                    total += self.filled_table[k][j]
+            return total
+        else:
+            return sum([sum(x) for x in self.filled_table])
 
     def calculate_global_entropy(self):
         
-        self.get_sum_table_values()
+        self.total_counter = self.get_sum_table_values()
+        self.final_entropy = 0
+        if self.is_hash:
+            # for each row
+            for x in self.filled_table:
 
-        # for each row
-        for x in self.filled_table:
+                # calculate the probabilities
+                probs = self.calculate_each_probability(self.filled_table[x])
 
-            # calculate the probabilities
-            probs = self.calculate_each_probability(self.filled_table[x])
+                # calculate the entropy of the row
+                entropy_row = self.calculate_each_entropy(probs)
 
-            # calculate the entropy of the row
-            entropy_row = self.calculate_each_entropy(probs)
+                #print(str(x) + " -> " + str(filled_table[x]) + str(probs))
+                #print(str(x) + str(filled_table[x]) + str(probs) + str(entropy_row))
+                #print(entropy_row)
+                #print(sum(self.filled_table[x].values()))
 
-            #print(str(x) + " -> " + str(filled_table[x]) + str(probs))
-            #print(str(x) + str(filled_table[x]) + str(probs) + str(entropy_row))
-            #print(entropy_row)
-            #print(sum(self.filled_table[x].values()))
+                # calculate the entropy of the entire text
+                self.final_entropy += sum(self.filled_table[x].values()) / self.total_counter * entropy_row
 
-            # calculate the entropy of the entire text
-            self.final_entropy += sum(self.filled_table[x].values())/self.total_counter * entropy_row
+                #print(sum(probs)/len(probs) * entropy_row)
+        else:
+            for row in self.filled_table:
+                probs = self.calculate_each_probability(row)
+                entropy_row = self.calculate_each_entropy(probs)
+                self.final_entropy += sum(row) / self.total_counter * entropy_row
 
-            #print(sum(probs)/len(probs) * entropy_row)
 
 if __name__== "__main__":
     
@@ -168,7 +181,7 @@ if __name__== "__main__":
     k = 1 if len(sys.argv) < 3 else int(sys.argv[2])
 
     # new fcm model
-    fcm = Fcm(filename, k)
+    fcm = Fcm(filename, k, 1)
 
     # get the entropy
     fcm.calculate_global_entropy()
