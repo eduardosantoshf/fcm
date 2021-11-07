26a29120c9c1bdcc67c73af99f84423889474a6d
BrunosBastos <Brunosb@ua.pt>
2021-11-07 22:12:38 +0000

    more charts

--- src/charts.py
+++ src/charts.py
@@ -1,13 +1,15 @@
 import matplotlib.pyplot as plt
 from fcm import Fcm
-
+from generator import Generator
+import os
+import shutil
 
 ks = [1,2,3,4,5]
 alphas = [0.0001, 0.001, 0.1, 1, 2]
 entropies = [[0]*len(alphas) for _ in range(len(ks))]
 
 for ki, k in enumerate(ks):
-    fcm = Fcm("../example/example.txt", k, 0)
+    fcm = Fcm("../example/example.txt", k, 0, 0)
     for a,alpha in enumerate(alphas):
         fcm.alpha = alpha
         entropies[ki][a] = fcm.calculate_global_entropy()
@@ -26,8 +28,76 @@ plt.xlabel("alpha")
 plt.ylabel("entropy")
 plt.show()
 
-for k in range(len(ks)):
-    plt.plot(ks, [entropies[k][i] for i in range(len(alphas))], label="k="+str(k))
+
+# entropia entre dois ficheiros diferentes
+entropies1 = [[0]*len(alphas) for _ in range(len(ks))]
+for ki, k in enumerate(ks):
+    fcm = Fcm("../example/example1.txt", k, 0, 0)
+    for a,alpha in enumerate(alphas):
+        fcm.alpha = alpha
+        entropies1[ki][a] = fcm.calculate_global_entropy()
+
+plt.plot(ks, [entropies1[i][0] for i in range(len(ks))])
+plt.title("entropy change relative to k with alpha=" + str(alphas[0]))
+plt.xlabel("k")
+plt.ylabel("entropy")
+plt.show()
+
+plt.plot(alphas, [entropies1[2][i] for i in range(len(alphas))])
+plt.title("entropy change relative to alpha with k=" + str(ks[2]))
+plt.xlabel("alpha")
+plt.ylabel("entropy")
+plt.show()
+
+
+for ki, k in enumerate(ks):
+    plt.plot(ks, [entropies[ki][i] for i in range(len(alphas))], label="k="+str(k))
 
 plt.legend()
 plt.show()
+
+# comparação da entropia de textos gerados com base no tamanho
+os.mkdir("./.tmp/")
+fcm = Fcm("../example/example.txt", 3, 0.1, 0)
+sizes = [10000, 50000, 100000, 500000, 1000000, 5000000, 10000000]
+gen = Generator(fcm)
+entropies = []
+for size in sizes:
+    filename = "./.tmp/gen" + str(size) + ".txt"
+    gen.generate_to_file(filename, size, "1:1")
+    n_fcm = Fcm(filename, 3, 0.1, 0)
+    entropies.append(n_fcm.calculate_global_entropy())
+
+shutil.rmtree("./.tmp/")
+
+plt.plot(sizes, entropies, label="generated")
+plt.plot(sizes, [fcm.calculate_global_entropy() for _ in range(len(sizes))], label="original")
+plt.legend()
+plt.xlabel("number of generated chars")
+plt.ylabel("entropy")
+plt.title("Variance of entropy based on the number of chars of the text")
+plt.show()
+
+
+# importancia do alpha na geração de texto
+os.mkdir("./.tmp/")
+entropies = []
+alphas = [0.0001, 0.001, 0.1, 0.3, 0.5, 0.7, 1]
+size = 100000
+fcm = Fcm("../example/example.txt", 3, 0, 0)
+for alpha in alphas:
+    fcm.alpha = alpha
+    gen = Generator(fcm)
+    filename = "./.tmp/gen" + str(size) + ".txt"
+    gen.generate_to_file(filename, size, "1:1")
+    n_fcm = Fcm(filename, 3, alpha, 0)
+    entropies.append(n_fcm.calculate_global_entropy())
+
+shutil.rmtree("./.tmp/")
+
+plt.plot(alphas, entropies)
+plt.xlabel("alpha")
+plt.ylabel("entropy")
+plt.title("Variance of entropy of generated text based on the alpha")
+plt.show()
+
--- src/generator.py
+++ src/generator.py
@@ -3,34 +3,43 @@ import sys
 import random
 import argparse
 
-def main(size, seq):
+class Generator:
 
-    if not seq:
-        seq = ""
-        for i in range(k):
-            seq += fcm.alphabet[random.randint(0, fcm.alphabet_size - 1)]
+    def __init__(self, fcm):
+        self.fcm = fcm
+    
+    def generate(self, size=100, seq=None):
+        if not seq:
+            seq = ""
+            for i in range(k):
+                seq += self.fcm.alphabet[random.randint(0, self.fcm.alphabet_size - 1)]
 
-    if len(seq) != k:
-        print("the initial sequence must have the size k")
-        exit(1)
+        if len(seq) != self.fcm.k:
+            print("the initial sequence must have the size k")
+            exit(1)
 
-    text = seq
-    for i in range(size):
-        row = fcm.get_table_row(seq)
-        total = fcm.get_row_sum(seq) + fcm.alphabet_size * fcm.alpha
-        r = random.random() * (total)
-        for i, v in enumerate(row):
-            r -= v + fcm.alpha
-            if r <= 0:
-                text += fcm.alphabet[i]
-                break
-        
-        seq = seq[1:] + text[-1]
+        text = seq
+        for i in range(size):
+            row = self.fcm.get_table_row(seq)
+            total = self.fcm.get_row_sum(seq) + self.fcm.alphabet_size * self.fcm.alpha
+            r = random.random() * (total)
+            for i, v in enumerate(row):
+                r -= v + self.fcm.alpha
+                if r <= 0:
+                    text += self.fcm.alphabet[i]
+                    break
+            
+            seq = seq[1:] + text[-1]
 
-    print(text)
+        return text
+
+    def generate_to_file(self, filename, size=100, seq=None):
+        f = open(filename, "w")
+        f.write(self.generate(size, seq))
+        f.close()
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser = argparse.ArgumentParser(description='Generate some text.')
     parser.add_argument("file", metavar="file", type=str)
     parser.add_argument('-k', type=int,
                     help='size of the sequence', default=1)
@@ -56,6 +65,6 @@ if __name__ == "__main__":
 
     # new fcm model
     fcm = Fcm(filename, k, alpha)
-
-    main(size, seq)
+    generator = Generator(fcm)
+    print(generator.generate(size, seq))
     
